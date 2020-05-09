05a0025584f19928c02d2005d22dcdddb004aa0b
Alvaro Centeno <alvaroc0689@gmail.com>
2020-05-09 16:37:55 -0700

    added input and submit btn to html. JS has ajax with API to call forecast with zip code

--- app.js
+++ app.js
@@ -1,1 +1,31 @@
-console.log("hello world")
+$(document).ready(function () {
+    var userInput = "";
+    $("#submitBtn").on("click", function (e) {
+        e.preventDefault();
+
+        let userInput = $("#userInput").val();
+
+        console.log(userInput);
+
+        $("#userInput").val("");
+
+        var api_key = "3dababe5876bb70212d76fb0d2f5aa26";
+
+        $.ajax({
+            type: "GET",
+            url: `https://api.openweathermap.org/data/2.5/weather?zip=${userInput}&units=imperial&appid=${api_key}`,
+            dataType: "json",
+        }).then(function (response) {
+
+            var cityName = response.name;
+            var kelvin = Math.floor(response.main.temp_max);
+
+            $("body").prepend(`<h1>The temperatur is ${kelvin} in ${cityName}</h1> `);
+        });
+
+
+
+
+    })
+
+})
